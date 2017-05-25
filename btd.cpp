4256d47a76f7c9029b28182bf037d66033dd3879
jakeajames <jakeashacks@gmail.com>
2017-05-25 19:02:13 +0200

    Update btd.cpp

--- btd.cpp
+++ btd.cpp
@@ -4,13 +4,33 @@
 using namespace std;
 
 char bin[8];
+char str[8];
 int nr = 0;
 
 int main() {
  printf("Input a binary value\n");
  gets(bin);
- for (int i = 0; i < 8; i++) {
-   if (bin[i] == '1') {
+ 
+
+ for (int i = 7; i >= 0; i--) {
+
+     if (bin[i] != '0' && bin[i] != '1') {
+         if (i == 7) str[0] =  '0';
+         if (i == 6) str[1] =  '0';
+         if (i == 5) str[2] =  '0';
+         if (i == 4) str[3] =  '0';
+         if (i == 3) str[4] =  '0';
+         if (i == 2) str[5] =  '0';
+         if (i == 1) str[6] =  '0';
+         if (i == 0) str[7] =  '0';
+     }
+    }
+       char fbin[8] = strcat(str, bin);
+   
+      
+       
+        for (int i = 7; i >= 0; i--) {
+   if (fbin[i] == '1') {
      
        if  (i == 0) {
        nr += 128;
@@ -47,5 +67,5 @@ int main() {
    }
 
  }
- cout << "Decimal value is" << nr;
+ cout << "Decimal value is: " << nr;
 }
